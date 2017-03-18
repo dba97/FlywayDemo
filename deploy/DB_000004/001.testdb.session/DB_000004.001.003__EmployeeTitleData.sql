ba204408b11952f78ee334aaf6afd60762a3fcc1
Nitin Thakral <nitinthakral@yahoo.com>
2017-03-18 16:31:00 -0400

    Fourth release for flyway demo

---
+++ deploy/DB_000004/001.testdb.session/DB_000004.001.003__EmployeeTitleData.sql
@@ -0,0 +1,8 @@
+insert into title values('Vice President')
+insert into title values('Sr. Director')
+insert into title values('Director')
+insert into title values('Manager')
+insert into title values('Associate Manager')
+insert into title values('Team Manager')
+
+
