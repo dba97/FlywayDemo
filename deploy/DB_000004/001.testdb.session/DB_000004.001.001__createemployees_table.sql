4707d4316689b1a49e6c7cf35f34240ba7d41c41
Nitin Thakral <nitinthakral@yahoo.com>
2017-03-18 20:54:53 -0400

    Fourth Release

---
+++ deploy/DB_000004/001.testdb.session/DB_000004.001.001__createemployees_table.sql
@@ -0,0 +1,7 @@
+create table employees
+(
+id int not null identity,
+first_name varchar(40) not null ,
+last_name varchar(40) not null ,
+primary key (id)
+);
---
+++ deploy/DB_000004/001.testdb.session/DB_000004.001.002__createtitle_table.sql
@@ -0,0 +1,7 @@
+create table title
+(
+id int not null identity,
+title varchar(100) not null,
+primary key (id)
+)
+go
